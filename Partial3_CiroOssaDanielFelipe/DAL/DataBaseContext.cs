d422ab6688301cab1d268cce136d0f490655e4aa
Daniel Ciro <danielciro312@gmail.com>
2023-11-11 22:35:15 -0500

    Database creation

---
+++ Partial3_CiroOssaDanielFelipe/DAL/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Partial3_CiroOssaDanielFelipe.DAL.Entities;
+
+namespace Partial3_CiroOssaDanielFelipe.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(c => c.Id).IsUnique();
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ Partial3_CiroOssaDanielFelipe/DAL/Entities/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Partial3_CiroOssaDanielFelipe.DAL.Entities
+{
+    public class Ticket : Entity
+    {
+        [Required]
+        public DateTime? UseDate { get; set; }
+
+        public bool IsUsed { get; set; }
+
+        public String? EntranceGate { get; set; }
+    }   
+}
