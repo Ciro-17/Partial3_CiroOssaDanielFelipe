b76c85b8f66df985da567828b05f22b60b8fd1b9
Daniel Ciro <danielciro312@gmail.com>
2023-11-12 00:05:48 -0500

    Creation Seeder

---
+++ Partial3_CiroOssaDanielFelipe/DAL/SeederDB.cs
@@ -0,0 +1,42 @@
+﻿namespace Partial3_CiroOssaDanielFelipe.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBaseContext _context;
+
+        public SeederDB(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            await SellTicketsAsync();   
+        }
+
+        #region Private Methos
+
+        private async Task SellTicketsAsync()
+        {
+            if (!_context.Tickets.Any()) 
+            {
+                for (int i = 0; i <= 1000; i++)
+                {
+                    _context.Tickets.Add(new Entities.Ticket
+                    {
+
+                        IsUsed = false,
+                        UseDate = null,
+                        EntranceGate = null,
+
+                    }
+                    );
+                }
+            }
+        }
+
+        #endregion
+    }
+}
